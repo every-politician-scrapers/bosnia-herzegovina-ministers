c30b8c5849607d7d48e7373e0d8c35d38541eca5
Tony Bowden <tony@tmtm.com>
2022-01-15 09:57:41 +0200

    compare enwiki Federation Presidents

---
+++ sources/federation-president/scraper.rb
@@ -0,0 +1,29 @@
+#!/bin/env ruby
+# frozen_string_literal: true
+
+require 'every_politician_scraper/scraper_data'
+require 'pry'
+
+class OfficeholderList < OfficeholderListBase
+  decorator RemoveReferences
+  decorator UnspanAllTables
+  decorator WikidataIdsDecorator::Links
+
+  def header_column
+    'Portrait'
+  end
+
+  class Officeholder < OfficeholderBase
+    def columns
+      %w[colour image name ethnicity start end].freeze
+    end
+
+    def tds
+      noko.css('td,th')
+    end
+
+  end
+end
+
+url = ARGV.first
+puts EveryPoliticianScraper::ScraperData.new(url, klass: OfficeholderList).csv
